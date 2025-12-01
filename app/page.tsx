28635957a7ac910fa423a77b5cb473f6a7b0eea9
Alpha_elv <Carterthree73@gmail.com>
2025-12-01 13:01:29 +0300

    Fixed JSX and added story to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
-  return (
+  return(
     <main className="p-8">
-      <h1 className="text-4xl font-bold text-center text-blue-600 dark:text-white">
-        Alpha_elv is rising from Kericho 🌍🔥
-      </h1>
-      <p className="mt-4 text-lg text-gray-700 dark:text-gray-300 text-center">
-        Built with resilience, faith, and code. From the forest to the future.
-      </p>
-    </main>
-  )
-}
+       <h1 className="text-4xl font-bold text-centre text-blue-6
+         Alpha_elv is rising from Kericho
+       <h1/>
+       <p className="mt-4 text-lg text gray-700 dark:text gray-300">
+         He's unapologetic! A man built with resilience, faith ,and code.From the forest to the future,complex!
+       <p/>
+    <main/>
+    )
+}     
